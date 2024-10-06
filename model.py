9f49e8c5b6a58ecfcbadea86db7e0048fe73ef6d
Silicon23 <121290367+Silicon23@users.noreply.github.com>
2024-10-07 00:58:31 +0800

    transformers in embedding sequence processing

--- model.py
+++ model.py
@@ -4,21 +4,14 @@ import sys
 import torch
 import torch.nn as nn
 
-spec = importlib.util.spec_from_file_location("byol_a", os.path.join(os.path.dirname(__file__), "byol-a/byol_a", "__init__.py"))
-byol_a = importlib.util.module_from_spec(spec)
-sys.modules["byol_a"] = byol_a
-spec.loader.exec_module(byol_a)
-
-from byol_a.models import AudioNTT2020
-
 class MusicEmotionRecognition(nn.Module):
-    def __init__(self, device = torch.device("cuda"), embedding_dim = 2048, encoder_weights = "byol-a/pretrained_weights/AudioNTT2020-BYOLA-64x96d2048.pth"):
+    def __init__(self, embedding_dim = 2048):
         super(MusicEmotionRecognition, self).__init__()
-        self.encoder = AudioNTT2020(d=embedding_dim)
-        self.encoder.load_weight(encoder_weights, device)
-        for param in self.encoder.parameters():
-            param.requires_grad = False
-        self.emotion_layer = nn.Sequential(
+        self.transformer_layer = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=embedding_dim, nhead=8, dim_feedforward=2048, activation='relu'),
+            num_layers=2
+        )
+        self.output_layer = nn.Sequential(
             nn.Linear(embedding_dim, 1024),
             nn.ReLU(),
             nn.Linear(1024, 128),
@@ -30,4 +23,6 @@ class MusicEmotionRecognition(nn.Module):
         )
 
     def forward(self, x):
-        return self.emotion_layer(self.encoder(x))
+        x = self.transformer_layer(x)
+        x = x.mean(dim=-2)  # Aggregate the sequence
+        return self.output_layer(x)
